cf0b4e8cc1e37dc98dcf298bedfca45530f3ef32
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-07-20 15:34:59 -0400

    formed connection

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,8 @@
-const { }
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB';
+
+connect(connectionString);
+
+module.exports = connection;
